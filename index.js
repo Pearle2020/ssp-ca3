217ae0dd766c31995897c9162456039759e3c9b5
Pearle2020 <tchatchoualucresse@Gmail.com>
2022-01-05 18:18:12 +0000

    styling the boostrap

--- index.js
+++ index.js
@@ -9,23 +9,28 @@ const   http = require('http'),
 const router = express();
 const server = http.createServer(router);
 
-router.get('/', function(req, res){
-res.writeHead(200, {'content-Typer': 'text/html'});
-let xml = fs.readFileSync('Colonnelbakery.xml' , 'utf-8'),
-    xsl =fs.readFileSync('Colonnelbakery.xsl' , 'utf-8');
 
+router.use(express.static(path.resolve(__dirname,'views'))); 
+
+router.get('/get/html', function(req, res){
+res.writeHead(200, {'content-Typer': 'text/html'});
+let xml = fs.readFileSync('Colonnelbakery.xml' , 'utf8'),
+    xsl =fs.readFileSync('Colonnelbakery.xsl' , 'utf8');
+    console.log(xml);
+    console.log(xsl);
     let doc = xmlParse(xml),
         stylesheet = xmlParse(xsl);
+        console.log(doc);
+    console.log(stylesheet);
+
 
     let result = xsltProcess(doc, stylesheet);
-
+    console.log(result);
     res.end(result.toString());
 
 });
 
-server.listen(process.env.Port|| 3000, process.env.IP|| "0.0.0.0", function()
-{
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
     const addr = server.address();
-    console.log("Server listening at" , addr.address + ":" + addr.port)
-
+    console.log("Server listening at", addr.address + ":" + addr.port)
 });
