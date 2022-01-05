8531d8803b2cee56623bd2e6ccaba18d4c0d08ba
Pearle2020 <tchatchoualucresse@Gmail.com>
2022-01-05 16:05:51 +0000

    creating the modules

--- index.js
+++ index.js
@@ -2,6 +2,30 @@ const   http = require('http'),
         path = require('path'),
         express = require('express'),
         fs = require('fs'),
-        xmlParse = require('xslt-precessor').xmlParse,
-        xsltProcess = require('xslt-precessor').xsltProcess,
-        xml2js = require('xml2js')
+        xmlParse = require('xslt-processor').xmlParse,
+        xsltProcess = require('xslt-processor').xsltProcess,
+        xml2js = require('xml2js');
+
+const router = express();
+const server = http.createServer(router);
+
+router.get('/', function(req, res){
+res.writeHead(200, {'content-Typer': 'text/html'});
+let xml = fs.readFileSync('Colonnelbakery.xml' , 'utf-8'),
+    xsl =fs.readFileSync('Colonnelbakery.xsl' , 'utf-8');
+
+    let doc = xmlParse(xml),
+        stylesheet = xmlParse(xsl);
+
+    let result = xsltProcess(doc, stylesheet);
+
+    res.end(result.toString());
+
+});
+
+server.listen(process.env.Port|| 3000, process.env.IP|| "0.0.0.0", function()
+{
+    const addr = server.address();
+    console.log("Server listening at" , addr.address + ":" + addr.port)
+
+});
